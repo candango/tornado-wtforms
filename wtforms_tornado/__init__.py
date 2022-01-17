ab943fcf31d042030fc13f97675c3f3be794cfe3
Flavio Garcia <piraz@candango.org>
2022-01-17 11:24:41 -0500

    Removed references to wtforms compat.
    Also added tornado wtforms project header to the files.
    
    Refs: #1
    Fixs: #3

--- wtforms_tornado/__init__.py
+++ wtforms_tornado/__init__.py
@@ -1,13 +1,26 @@
-# -*- coding: utf-8 -*-
-"""WTForms extensions for Tornado."""
-from wtforms import compat
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2022 Flávio Gonçalves Garcia
+# Copyright 2013-2014 Jorge Puente Sarrín
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 
 version_tuple = (0, 0, 2)
 
 
 def get_version_string():
-    if isinstance(version_tuple[-1], compat.string_types):
+    if isinstance(version_tuple[-1], str):
         return '.'.join(map(str, version_tuple[:-1])) + version_tuple[-1]
     return '.'.join(map(str, version_tuple))
 
--- wtforms_tornado/form.py
+++ wtforms_tornado/form.py
@@ -1,9 +1,22 @@
-# -*- coding: utf-8 -*-
-import re
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2022 Flávio Gonçalves Garcia
+# Copyright 2013-2014 Jorge Puente Sarrín
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from tornado import escape
 from wtforms import form
-from wtforms.compat import text_type
 from wtforms_tornado.i18n import TornadoTranslations
 
 
